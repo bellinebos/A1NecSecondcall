97a6508ef44aac9c3c10f6022fbc56bed177aa1f
bellinebos <belline.bos@gmail.com>
2025-01-31 15:27:44 +0100

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,22 @@
 data_original = pd.read_csv('data.csv')
+
+# Show mean, median, min and max values
+print(data_original.describe().loc[['mean', '50%', 'min', 'max']])
+
+# Filter only numeric columns
+numeric_df = data_original.select_dtypes(include=['number'])
+
+# Plot the boxplots
+plt.subplots(figsize=(20, 8))
+
+count = 1
+for i in numeric_df.columns:
+    plt.subplot(2, 10, count)  # Adjust the layout (2 rows, 10 columns)
+    sns.boxplot(data=numeric_df[i])
+    plt.title(i, fontsize=12)
+    count += 1
+
+plt.suptitle('Boxplots of all numeric variables of the dataset', fontsize=16)
+plt.subplots_adjust(top=0.85)  # Adjust the title space to avoid overlap
+plt.tight_layout()  # Ensure that subplots do not overlap
+plt.show()
